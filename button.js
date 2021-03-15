3c958dcc6f02e907b36f5b615c67c8c5aba10261
sajibvx <77675025+sajibvx@users.noreply.github.com>
2021-03-16 00:13:29 +0600

    Button.js

---
+++ button.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><button onclick="alert('How Can I help you?')"> Click Me   </button>
+
+<button id=button> Click Me</button>
+
+<button id=button2> Hello</button>
+
+<script>
+
+document.getElementById("button").onclick=function(){
+
+alert("You have just Clicked Button");
+
+}
+document.getElementById("button2").onmouseover= function(){
+
+alert("Hello World");
+
+}
+
+</script>
+
+
+
+
+
+</body>
+</html>
